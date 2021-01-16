28ddeaf6a10f09ba193d31b86d621389f2aea735
buddinisansala <nisansala940914@gmail.com>
2021-01-16 20:49:09 +0530

    added ems assignment

---
+++ ems/employee/Employee.java
@@ -0,0 +1,9 @@
+package employee;
+
+public class Employee {
+
+    public void print(){
+        System.out.println("I am an employee");
+    }
+    
+}
---
+++ ems/manager/Manager.java
@@ -0,0 +1,9 @@
+package manager;
+
+public class Manager{
+
+    public void print(){
+        System.out.println("I am a manager");
+    }
+    
+}
